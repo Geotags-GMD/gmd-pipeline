e724945f067eedddec90052555c1806b28f492a3
tatsmenot <francearthurtating@gmail.com>
2026-07-22 10:25:55 +0800

    feat: implement custom VitePress theme with global component registration

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import Layout from './Layout.vue'
-import './custom.css'
 
 import QGISRepositoryCard from './components/QGISRepositoryCard.vue'
 import LatestReleaseCard from './components/LatestReleaseCard.vue'
